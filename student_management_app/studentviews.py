521c9239e9ed1536393776c88b06f349d7a61368
Ashish Maharjan <amaharjan1032@gmail.com>
2020-07-08 00:38:22 +0545

    Student home done

--- student_management_app/studentviews.py
+++ student_management_app/studentviews.py
@@ -6,7 +6,25 @@ from django.contrib import messages
 from django.urls import reverse
 
 def student_home(request):
-    return render(request,'student_template/student_home.html')
+    student_obj = Students.objects.get(admin=request.user.id)
+    attendance_total = AttendanceReport.objects.filter(student_id=student_obj).count()
+    attendance_present = AttendanceReport.objects.filter(student_id=student_obj, status=True).count()
+    attendance_absent = AttendanceReport.objects.filter(student_id=student_obj, status=False).count()
+    course = Courses.objects.get(id=student_obj.course_id.id)
+    subjects = Subjects.objects.filter(course_id=course).count()
+
+    subject_name = []
+    data_present = []
+    data_absent = []
+    subject_date = Subjects.objects.filter(course_id=student_obj.course_id)
+    for subject in subject_date:
+        attendance = Attendance.objects.filter(subject_id=subject.id)
+        attendance_present_count = AttendanceReport.objects.filter(attendance_id__in=attendance, status=True, student_id=student_obj.id).count()
+        attendance_absent_count = AttendanceReport.objects.filter(attendance_id__in=attendance, status=False, student_id=student_obj.id).count()
+        subject_name.append(subject.subject_name)
+        data_present.append(attendance_present_count)
+        data_absent.append(attendance_absent_count)
+    return render(request,'student_template/student_home.html',{'total_attendance':attendance_total, 'attendance_present':attendance_present, 'attendance_absent':attendance_absent, 'subjects':subjects, 'data_name':subject_name, 'data1':data_present, 'data2':data_absent})
 
 
 def student_view_attendance(request):
